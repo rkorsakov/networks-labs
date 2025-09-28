6b3d9f6f443198af812295114fa380a807997fe3
Roman <roma3io@mail.ru>
2025-09-28 13:20:27 +0700

    handlers TODO

--- snake-game/internal/network/handler.go
+++ snake-game/internal/network/handler.go
@@ -1,5 +1,78 @@
 package network
 
-import "net"
+import (
+	"github.com/golang/protobuf/proto"
+	"log"
+	"net"
+	prt "snake-game/internal/proto/gen"
+)
 
-func (m *Manager) handleMessage(msg []byte, addr *net.UDPAddr) {}
+type GameMessage struct {
+}
+
+func (m *Manager) handleMessage(data []byte, addr *net.UDPAddr) {
+	var msg prt.GameMessage
+	if err := proto.Unmarshal(data, &msg); err != nil {
+		log.Printf("Error unmarshaling message: %v", err)
+		return
+	}
+	log.Printf("Received message seq=%d from %s", msg.MsgSeq, addr)
+	switch {
+	case msg.GetPing() != nil:
+		log.Println("Got PING message")
+		m.handlePing(&msg, addr)
+
+	case msg.GetSteer() != nil:
+		log.Println("Got STEER message")
+		m.handleSteer(&msg, addr)
+
+	case msg.GetAck() != nil:
+		log.Println("Got ACK message")
+		m.handleAck(&msg, addr)
+
+	case msg.GetState() != nil:
+		log.Println("Got STATE message")
+		m.handleState(&msg, addr)
+
+	case msg.GetAnnouncement() != nil:
+		log.Println("Got ANNOUNCEMENT message")
+		m.handleAnnouncement(&msg, addr)
+
+	case msg.GetJoin() != nil:
+		log.Println("Got JOIN message")
+		m.handleJoin(&msg, addr)
+
+	case msg.GetError() != nil:
+		log.Println("Got ERROR message")
+		m.handleError(&msg, addr)
+
+	case msg.GetRoleChange() != nil:
+		log.Println("Got ROLE_CHANGE message")
+		m.handleRoleChange(&msg, addr)
+
+	case msg.GetDiscover() != nil:
+		log.Println("Got DISCOVER message")
+		m.handleDiscovery(&msg, addr)
+
+	default:
+		log.Printf("Unknown message type from %s", addr)
+	}
+}
+
+func (m *Manager) handlePing(msg *prt.GameMessage, addr *net.UDPAddr) {}
+
+func (m *Manager) handleSteer(msg *prt.GameMessage, addr *net.UDPAddr) {}
+
+func (m *Manager) handleAck(msg *prt.GameMessage, addr *net.UDPAddr) {}
+
+func (m *Manager) handleDiscovery(msg *prt.GameMessage, addr *net.UDPAddr) {}
+
+func (m *Manager) handleJoin(msg *prt.GameMessage, addr *net.UDPAddr) {}
+
+func (m *Manager) handleState(msg *prt.GameMessage, addr *net.UDPAddr) {}
+
+func (m *Manager) handleAnnouncement(msg *prt.GameMessage, addr *net.UDPAddr) {}
+
+func (m *Manager) handleError(msg *prt.GameMessage, addr *net.UDPAddr) {}
+
+func (m *Manager) handleRoleChange(msg *prt.GameMessage, addr *net.UDPAddr) {}
